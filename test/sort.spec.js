c769927d44695d059c750f0621c7f1c7c304d8cd
Maxim Kovalov <max.kovalov@gmail.com>
2013-06-06 14:41:56 +0300

    Convert tabs to spaces.

--- test/sort.spec.js
+++ test/sort.spec.js
@@ -1,29 +1,29 @@
 var unsorted_array
-  ,	sorted_array=[1,1,1,2,2,2,3,3,4,4,4,5,6,6,7,7,23,43,52,65,123];
+  , sorted_array=[1,1,1,2,2,2,3,3,4,4,4,5,6,6,7,7,23,43,52,65,123];
 
 describe("Sort",function(){
-	beforeEach(function(){
-		unsorted_array=[2,1,4,65,1,43,2,6,7,3,2,1,123,4,52,4,6,7,3,23,5];
-	});
+    beforeEach(function(){
+        unsorted_array=[2,1,4,65,1,43,2,6,7,3,2,1,123,4,52,4,6,7,3,23,5];
+    });
 
-	
-	it("buble sort should sort an array",function(){
-		var bubble_sort=require("../lib/sort/bubble-sort");
-		expect(bubble_sort(unsorted_array))
-			.toEqual(sorted_array);
+    
+    it("buble sort should sort an array",function(){
+        var bubble_sort=require("../lib/sort/bubble-sort");
+        expect(bubble_sort(unsorted_array))
+            .toEqual(sorted_array);
 
-		expect(unsorted_array)
-			.not.toEqual(sorted_array);
-	});
+        expect(unsorted_array)
+            .not.toEqual(sorted_array);
+    });
 
 
-	it('merge sort should sort an array',function(){
-		var merge_sort=require("../lib/sort/merge-sort");
-		expect(merge_sort(unsorted_array))
-			.toEqual(sorted_array);
+    it('merge sort should sort an array',function(){
+        var merge_sort=require("../lib/sort/merge-sort");
+        expect(merge_sort(unsorted_array))
+            .toEqual(sorted_array);
 
-		expect(unsorted_array)
-			.not.toEqual(sorted_array);
-	})
+        expect(unsorted_array)
+            .not.toEqual(sorted_array);
+    })
 
 });
